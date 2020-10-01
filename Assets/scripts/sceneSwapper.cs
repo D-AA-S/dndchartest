90c3adb658c67c53a755bd14ae69e649b77d1d10
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2020-09-30 18:34:47 -0700

    Added more scenes & prototype scene swapper

---
+++ Assets/scripts/sceneSwapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwapper : MonoBehaviour
+{
+    public void backButton()
+    {
+        Debug.Log("TO THE MAIN");
+        SceneManager.LoadScene("Main menu"); 
+    }
+    public void unimplemented()
+    {
+        SceneManager.LoadScene("Unimplemented");
+    }
+    public void settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void about()
+    {
+        SceneManager.LoadScene("About");
+    }
+    public void quit()
+    {
+
+    }
+}
