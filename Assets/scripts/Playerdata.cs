f81563d7ee86295340b75f7367fed6f2b2eb55c2
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2020-10-15 14:24:04 -0700

    Added playerdata

---
+++ Assets/scripts/Playerdata.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rollchar : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public class playerdata
+    {
+        public float ability_str;
+        public float ability_dex;
+        public float ability_con;
+        public float ability_int;
+        public float ability_wis;
+        public float ability_cha;
+    }
+}
