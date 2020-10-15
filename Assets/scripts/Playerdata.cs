a0839e8af4d30a931af8730df5a37b574b3fe3ca
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2020-10-15 14:36:05 -0700

    fixed issue with mismatched script names

--- Assets/scripts/Playerdata.cs
+++ Assets/scripts/Playerdata.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Rollchar : MonoBehaviour
+public class playerdata : MonoBehaviour
 {
 
     // Start is called before the first frame update
@@ -17,13 +18,30 @@ public class Rollchar : MonoBehaviour
         
     }
 
-    public class playerdata
+    [Serializable]
+    public class playerinfo
     {
+        //Ability scores
         public float ability_str;
         public float ability_dex;
         public float ability_con;
         public float ability_int;
         public float ability_wis;
         public float ability_cha;
+
+        //character choices
+        public string character_name;
+        public string race;
+        public string playerclass;
+        public string alignment;
+
+        //substats
+        public int maxxp, currentxp;
+        public int maxhp, currenthp;
+        public int Armourclass;
+        public int walkspeed, runspeed, jumpheight;
+
+        //inventory
+        public List<String> Inventory; 
     }
 }
