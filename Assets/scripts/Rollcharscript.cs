08ec98a7009ac1e3b49e3738f74285a49414c5ef
Daniel Albertazzi Silva <dsilva5211@sdsu.edu>
2020-10-15 21:48:58 -0700

    Added diceroll script

--- Assets/scripts/Rollcharscript.cs
+++ Assets/scripts/Rollcharscript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class rollcharscript : MonoBehaviour
 {
+    int d6Total = 3;
+    int d4Total = 3;
+    List<float> d4roll;
+    List<float> d6roll;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,22 @@ public class rollcharscript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public float statroller()
+    {
+        float total;
+        for (int i = 0; i < d6Total; i++)
+        {
+            d6roll.Add(Random.Range(1, 6));
+        }
+        for (int i = 0; i < d4Total; i++)
+        {
+            d6roll.Add(Random.Range(1, 4));
+        }
+        d6roll.Sort();
+        d4roll.Sort();
+        total = d6roll[1] + d6roll[2] + d4roll[1] + d4roll[2];
+        return total;
     }
 }
